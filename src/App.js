524935061f355d12faefd8ced13e330cc0c5edbf
afandiaziz <afandiaziz46@gmail.com>
2022-07-17 18:56:51 +0700

    metro.config.js react-native-svg-transformer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {View, StatusBar, Text} from 'react-native';
 
 const App = () => {
-  return (
-    <View>
-      <StatusBar barStyle="dark-content" />
-      <Text>Hello World!</Text>
-    </View>
-  );
+    return (
+        <View>
+            <StatusBar barStyle="dark-content" />
+            <Text>Hello World!</Text>
+        </View>
+    );
 };
 export default App;
