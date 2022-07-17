e9b9278eabfa4c3c3adea39cd689ac55fcc48f0a
afandiaziz <afandiaziz46@gmail.com>
2022-07-17 18:33:10 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StatusBar, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" />
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+export default App;
