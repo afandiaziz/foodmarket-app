1155b290726c032dff2e8783d08b3c158615321d
afandiaziz <afandiaziz46@gmail.com>
2022-07-17 22:32:51 +0700

    add react-navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {SplashScreen} from './pages';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
-    return <SplashScreen />;
+    return (
+        <NavigationContainer>
+            <SplashScreen />
+        </NavigationContainer>
+    );
 };
 export default App;
